7bfb5b6a9ba9f2ce7af8adbb03f4b36ad9746cef
m1kp <yarikasik@inbox.ru>
2024-08-17 22:41:47 +0300

    Update break.lua

--- break.lua
+++ break.lua
@@ -1,5 +1,5 @@
 --script for all!!
---version: 2.4 (now)
+--version: 2.5 (now)
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 local Window = OrionLib:MakeWindow({Name = "LadderBreaker - dolce milk obby", HidePremium = false, IntroEnabled = true, IntroText = "Loading..", SaveConfig = true, ConfigFolder = "OrionTest"})
@@ -18,109 +18,110 @@ _G.breakLadder3 = true
 _G.antiBlur = true
 _G.wlkSpeed = true
 _G.jummpPower = true
+_G.brkspeed = brkspd
 --fnc
 function brkLdr()
     while _G.breakLadder == true do
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(87.624, 3.999, -250.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(90.624, 7.999, -255.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(95.624, 12.999, -260.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(100.624, 15.999, -263.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(110.624, 20.999, -265.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(120.624, 30.999, -256.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(127.624, 35.999, -250.481)
-        wait()
+        wait(_G.brkspeedspd)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(125.624, 40.999, -240.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(120.624, 45.999, -232.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(113.624, 50.999, -227.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(104.624, 56.999, -227.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(97.624, 60.999, -226.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(94.624, 63.999, -230.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(90.624, 70.999, -242.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(88.624, 72.999, -245.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(88.624, 74.999, -248.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(87.624, 76.999, -250.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(88.624, 80.999, -259.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(90.624, 81.999, -259.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(92.624, 82.999, -259.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(96.624, 86.999, -261.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(100.624, 88.999, -262.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(104.624, 89.999, -263.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(107.624, 91.999, -264.481)
-        wait()  
+        wait(_G.brkspeed)  
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(110.624, 92.999, -265.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(113.624, 94.999, -265.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(115.624, 95.999, -265.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(118.624, 98.999, -263.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(120.624, 99.999, -262.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(123.624, 101.999, -258.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(124.624, 104.999, -256.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(125.624, 105.999, -252.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(126.624, 108.999, -248.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(126.624, 110.999, -245.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(125.624, 110.999, -240.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(124.624, 115.999, -235.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(120.674, 119.999, -233.348)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(115.674, 122.999, -230.348)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(110.674, 125.999, -225.348)
-        wait()
+        wait(_G.brkspeedkspd)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(103.674, 130.999, -227.348)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(105.674, 127.999, -227.348)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(110.674, 127.999, -222.348)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(90.674, 135.999, -230.348)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(106.674, 130.999, -234.348)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(103.674, 129.999, -233.348)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(93.944, 134.999, -230.479)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(96.8, 132.999, -228.479)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(101.624, 129.999, -226.481)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(90.417, 137.999, -234.328)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(88.674, 140.999, -237.348)
-        wait()
+        wait(_G.brkspeed)
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(80.674, 146.999, -247.348)
     end
 end
@@ -130,23 +131,36 @@ function delblur()
                 game.Workspace.Camera.Blur:Destroy()
         end
 end
+--time
+function clcktme()
+        game.Lighting.ClockTime = _G.timeclock
+end
 --tab 1
 local Tab = Window:MakeTab({
 	Name = "Main",
         Image = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
---text
-Tab:AddParagraph("invisible breaking (need systembroken)","just attach other player (u can do: headsit, backpack, bang, stand, doggy, drag)")
 --toggle break
 Tab:AddToggle({
-	Name = "Break ladder (ultra fast)",
+	Name = "Break ladder",
 	Default = false,
 	Callback = function(Value)
 		_G.breakLadder = Value
         brkLdr()
 	end    
 })
+Tab:AddTextbox({
+	Name = "Speed",
+	Default = "",
+	TextDisappear = true,
+	Callback = function(brkspd)
+                _G.brkspeed = brkspd
+	end	  
+})
+--text
+Tab:AddParagraph("invisible breaking (need systembroken)","just attach other player (u can do: headsit, backpack, bang, stand, doggy, drag)")
+Tab:AddParagraph("Speed","lower num = speeder breaking, higher num = slower")
 --teleporttab
 local Tab7 = Window:MakeTab({
 	Name = "Teleport",
@@ -298,6 +312,20 @@ Tab3:AddToggle({
         delblur()
 	end    
 }) 
+--wlkspeed
+Tab4:AddButton({
+	Name = "Walk-speed default (16)",
+	Callback = function()
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+  	end    
+})
+--jumppower
+Tab4:AddButton({
+	Name = "Jump-power default (50)",
+	Callback = function()
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
+  	end    
+})
 --tab 4
 local Tab4 = Window:MakeTab({
 	Name = "Scripts",
@@ -325,6 +353,13 @@ Tab4:AddButton({
         loadstring(game:HttpGet('https://raw.githubusercontent.com/GhostPlayer352/Test4/main/Float'))("https://t.me/arceusxscripts")
   	end    
 })
+--shaders
+Tab4:AddButton({
+	Name = "Shaders",
+	Callback = function()
+loadstring(game:HttpGet(('https://pastefy.app/xXkUxA0P/raw'),true))("t.me/arceusxscripts")
+  	end    
+})
 --dex explorer v2
 Tab4:AddButton({
 	Name = "Dex Explorer v2",
@@ -333,6 +368,43 @@ Tab4:AddButton({
   	end    
 })
 --tab 5
+local Tabclock = Window:MakeTab({
+	Name = "Time",
+        Image = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tabclock:AddParagraph("Clock time","Only you can see night/morning/evening")
+
+Tabclock:AddButton({
+	Name = "Night",
+	Callback = function()
+      		game.Lighting.ClockTime = 0
+  	end    
+})
+
+Tabclock:AddButton({
+	Name = "Day",
+	Callback = function()
+      		game.Lighting.ClockTime = 12
+  	end    
+})
+
+Tabclock:AddButton({
+	Name = "Morning",
+	Callback = function()
+      		game.Lighting.ClockTime = 6
+  	end    
+})
+
+Tabclock:AddButton({
+	Name = "Evening",
+	Callback = function()
+      		game.Lighting.ClockTime = 18
+  	end    
+})
+
+--tabtt
 local Tab = Window:MakeTab({
 	Name = "Changelog",
         Image = "rbxassetid://4483345998",
@@ -344,6 +416,7 @@ Tab:AddParagraph("v2.1","added: teleport buttons in main tab")
 Tab:AddParagraph("v2.2","fixed teleport glitches, deleted server tab, added anti-admin tab and functions")
 Tab:AddParagraph("v2.3","added: faster breaking ladder, invis breaking (need systembroken), deleted two anti-admin functions (not working)")
 Tab:AddParagraph("v2.4","added: teleport tab")
+Tab:AddParagraph("v2.5","added: breaking speed changer, time changer, walk speed button, jump power button")
 Tab:AddParagraph("v3 soon...","spoiler: new breaking-ladder script (break FULL ladder), new scripts, changing clocktime")
 --tab 6
 local Tab6 = Window:MakeTab({
