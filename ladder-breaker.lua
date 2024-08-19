b4624c0defa29a25b8b3559a7a8deaadbf8b0b77
m1kp <yarikasik@inbox.ru>
2024-08-19 19:57:21 +0300

    Update ladder-breaker.lua

--- ladder-breaker.lua
+++ ladder-breaker.lua
@@ -1,5 +1,5 @@
 while true do
-    for i,v in pairs(game.Workspace) do
+    for i,v in pairs(game:GetDesctendants) do
         if v.Name == '61' then
             v.Vector3 = game.Players.LocalPlayer.HumanoidRootPart.Position
         end
