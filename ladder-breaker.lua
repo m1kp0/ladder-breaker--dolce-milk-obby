44993c1be364b0656817a47c3fec4918963632b3
m1kp <yarikasik@inbox.ru>
2024-08-19 19:54:01 +0300

    Update ladder-breaker.lua

--- ladder-breaker.lua
+++ ladder-breaker.lua
@@ -1,4 +1,4 @@
-for i,v in pairs(game:GetDescendants()) do
+for i,v in pairs(game.Workspace) do
     if v.Name == '61' then
         v.CFrame = game:GetService("Workspace").LYBLY_COCATb6969.HumanoidRootPart.Position
     end
