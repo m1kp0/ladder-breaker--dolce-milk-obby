6a763ebeeb138f8386a8208e1edc22ff54729247
m1kp <yarikasik@inbox.ru>
2024-08-19 20:20:18 +0300

    Update ladder-breaker.lua

--- ladder-breaker.lua
+++ ladder-breaker.lua
@@ -1,4 +1,4 @@
-local ds = game.Workspace.Stairs:GetDescendands()
+local ds = game.Workspace.Stairs
 local plr = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
 while true do
     ds.CFrame = plr
