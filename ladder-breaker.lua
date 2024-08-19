4f52732f9196c397c0d775a7b89d08d4573cedf7
m1kp <yarikasik@inbox.ru>
2024-08-19 19:56:38 +0300

    Update ladder-breaker.lua

--- ladder-breaker.lua
+++ ladder-breaker.lua
@@ -1,7 +1,7 @@
 while true do
     for i,v in pairs(game.Workspace) do
         if v.Name == '61' then
-            v.CFrame = game.Players.LocalPlayer.HumanoidRootPart.Position
+            v.Vector3 = game.Players.LocalPlayer.HumanoidRootPart.Position
         end
     end
 end
