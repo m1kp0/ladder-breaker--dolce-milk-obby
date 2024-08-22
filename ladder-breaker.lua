fd439d582cd45f42fe2ff50c7231753dbd681db4
m1kp <yarikasik@inbox.ru>
2024-08-23 00:06:59 +0300

    Update ladder-breaker.lua

--- ladder-breaker.lua
+++ ladder-breaker.lua
@@ -2,22 +2,26 @@ local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shl
 
 local Window = OrionLib:MakeWindow({Name = "LB - Key System", HidePremium = false, SaveConfig = true, IntroText = "LB - Welcome" ConfigFolder = "OrionTest"})
 
-_G.Key = Keye
+_G.Key = "m1kpeeGotTheMoney"
+_G.KeyInput = "string"
 
+function makeLB()
+    if sf_key == KeyInput then
+        game.GetService("CoreGui").Orion:Destroy()
+        wait(0.5)
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/m1kp0/scripts/main/ladder%20breaker%20-%20dmo.lua"))()
+    end
+end
 local TabKey = Window:MakeTab({
 	Name = "Key",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 
-function makeLB()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/m1kp0/scripts/main/ladder%20breaker%20-%20dmo.lua"))()
-end
-
 function CorrectKeyNotify()
     OrionLib:MakeNotification({
         Name = "Correct key",
-        Content = "You are logged as "..Player.Name..",
+        Content = "You are logged in as "..Player.Name..",
         Image = "rbxassetid://4483345998",
         Time = 5
     })
@@ -36,11 +40,24 @@ TabKey:AddTextbox({
 	Default = "",
 	TextDisappear = true,
 	Callback = function(Keye)
-		if _G.Key = "m1kpeeGotTheMoney" then
+        _G.KeyInput = Keye
+	end	  
+})
+
+TabKey:AddButton({
+	Name = "Check key",
+	Callback = function()
+      	if
+  	end    
+})
+
+TabKey:AddButton({
+	Name = "Copy link",
+	Callback = function()
+        if _G.KeyInput == _G.Key then
             CorrectKeyNotify()
             makeLB()
         else
             IncorrectKeyNotify()
-        end
-	end	  
+  	end    
 })
