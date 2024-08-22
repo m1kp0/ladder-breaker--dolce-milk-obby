be407396e5b7feec5199a79d192c2f9dba8de06c
m1kp <yarikasik@inbox.ru>
2024-08-22 23:49:46 +0300

    Update ladder-breaker.lua

--- ladder-breaker.lua
+++ ladder-breaker.lua
@@ -1,62 +1,46 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-Local Player = game.Players.LocalPlayer
-local Window = OrionLib:MakeWindow({Name = "LB - Key System", HidePremium = false, IntroEnabled = true, IntroText = "Loading..", SaveConfig = true, ConfigFolder = "OrionTest"})
 
-OrionLib:MakeNotification({
-	Name = "Logged In",
-	Content = "You are logged in as "..Player.Name..".",
-	Image = "rbxassetid://4483345998",
-	Time = 5
-})
+local Window = OrionLib:MakeWindow({Name = "LB - Key System", HidePremium = false, SaveConfig = true, IntroText = "LB - Welcome" ConfigFolder = "OrionTest"})
 
-_G.Key = "m1kpeeGotTheMoney"
-_G.KeyInput = "string"
+_G.Key = Keye
 
-function makeLadderBreaker()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/m1kp0/scripts/main/ladder%20breaker%20-%20dmo"))()
-end
-
-function EnteredCorrectKeyNotify()
-    OrionLib:MakeNotification({
-        Name = "Correct Key",
-        Content = "You have entered the correct key.",
-        Image = "rbxassetid://4483345998",
-        Time = 5
-    })
-end
-
-function EnteredIncorrectKeyNotify()
-    OrionLib:MakeNotification({
-        Name = "Inorrect Key",
-        Content = "You have entered the incorrect key.",
-        Image = "rbxassetid://4483345998",
-        Time = 5
-    })
-end
-
-local KeySystem = Window:MakeTab({
+local TabKey = Window:MakeTab({
 	Name = "Key",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 
-KeySystem:AddTextbox({
-	Name = "Enter Key",
+function makeLB()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/m1kp0/scripts/main/ladder%20breaker%20-%20dmo.lua"))()
+end
+
+function CorrectKeyNotify()
+    OrionLib:MakeNotification({
+        Name = "Correct key",
+        Content = "You are logged as "..Player.Name..",
+        Image = "rbxassetid://4483345998",
+        Time = 5
+    })
+end
+
+function IncorrectKeyNotify()
+    OrionLib:MakeNotification({
+        Name = "Incorrect key",
+        Content = "no.",
+        Image = "rbxassetid://4483345998",
+        Time = 10
+    })
+end
+TabKey:AddTextbox({
+	Name = "Enter key",
 	Default = "",
 	TextDisappear = true,
-	Callback = function(Value)
-		_G.KeyInput = Value
-	end	  
-})
-
-KeySystem:AddButton({
-	Name = "Check Key",
-	Callback = function()
-        if _G.KeyInput == _G.Key then
-            makeLadderBreaker()
-            EnteredCorrectKeyNotify()
+	Callback = function(Keye)
+		if _G.Key = "m1kpeeGotTheMoney" then
+            CorrectKeyNotify()
+            makeLB()
         else
-            EnteredIncorrectKeyNotify()
+            IncorrectKeyNotify()
         end
-    end    
+	end	  
 })
