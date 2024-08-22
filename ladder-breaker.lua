4882080f7b695c7a31bc9409f69e8b3d68bc62fc
m1kp <yarikasik@inbox.ru>
2024-08-23 00:55:25 +0300

    Update ladder-breaker.lua

--- ladder-breaker.lua
+++ ladder-breaker.lua
@@ -6,28 +6,6 @@ local Window = OrionLib:MakeWindow({Name = "LB - Key System", HidePremium = fals
 _G.Key = "m1kpeeGotTheMoney"
 _G.KeyInput = "e"
 
-function makeLB()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/m1kp0/scripts/main/ladder%20breaker%20-%20dmo.lua"))()
-end
-
-function correctKeyNotify()
-    OrionLib:MakeNotification({
-        Name = "Correct key",
-        Content = "Entered correct key!",
-        Image = "rbxassetid://4483345998",
-        Time = 5
-    })
-end
-
-function IncorrectKeyNotify()
-    OrionLib:MakeNotification({
-        Name = "Incorrect key",
-        Content = "Entered incorrect key!",
-        Image = "rbxassetid://4483345998",
-        Time = 8
-    })
-end
-
 local Tab = Window:MakeTab({
 	Name = "Main",
 	Icon = "rbxassetid://4483345998",
@@ -39,25 +17,14 @@ Tab:AddTextbox({
 	Default = "",
 	TextDisappear = true,
 	Callback = function(Value)
-		_G.KeyInput = Value
-        if _G.KeyInput == _G.Key then
-            correctKeyNotify()
-            makeLB()
-        else
-            IncorrectKeyNotify()
-        end
+        print(Value)
 	end	  
 })
 
 Tab:AddButton({
 	Name = "Check key",
 	Callback = function()
-      		if _G.KeyInput == _G.Key then
-                correctKeyNotify()
-                makeLB()
-            else
-                IncorrectKeyNotify()
-            end
+        print("sex")
   	end    
 })
 
